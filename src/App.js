953f8db1efbc9f7a1f787d9cc5c5ce7a9b5f0578
Dimitrije <dimitrijezdravkovic20@gmail.com>
2024-02-08 00:31:47 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PhoneSignIn from "./Components/PhoneSignIn";
+import Main from "./Components/Main.js";
 
 function App() {
   return (
-    <PhoneSignIn />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/sign-in" element={<PhoneSignIn />} />
+        <Route path="*" element={<Main />} />
+      </Routes>
+    </Router>
   )
 }
 
